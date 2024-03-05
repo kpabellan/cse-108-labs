48e040fe763f062e74551d4268f0f00f47022afa
Kobey Pabellan <pabellank@gmail.com>
2024-03-04 22:57:45 -0800

    Lab 5 functions

--- lab_5/scripts.js
+++ lab_5/scripts.js
@@ -1,13 +1,22 @@
-async function allGrades() {
-  const response = await fetch("https://amhep.pythonanywhere.com/grades", {
-    method: "GET",
+async function reqGrade(reqType, reqBody, studentName) {
+  const options = {
+    method: reqType,
     headers: {
       "Content-Type": "application/json",
     },
-  });
+  };
 
-  const gradesText = await response.text();
-  const gradesJSON = await JSON.parse(gradesText);
+  if (reqBody) {
+    options.body = JSON.stringify(reqBody);
+  }
+
+  if (studentName) {
+    studentName = "/" + studentName;
+  }
+
+  const response = await fetch(`https://amhep.pythonanywhere.com/grades${studentName}`, options);
+
+  const gradesJSON = await response.json();
 
   let table = "<tr><th>Student Name</th><th>Grade</th></tr>";
 
@@ -18,8 +27,32 @@ async function allGrades() {
   }
 
   document.getElementById("grades").innerHTML = table;
-}
+};
 
+// See all grades
 document.getElementById("allGrades").addEventListener("click", function() {
-  allGrades();
+  reqGrade("GET", null, "");
+});
+
+// See student grade
+document.getElementById("studentGrade").addEventListener("click", function() {
+  reqGrade("GET", null, "Han Solo");
+});
+
+// Create grade
+document.getElementById("createGrade").addEventListener("click", function() {
+  let grade = {"name": "Han Solo", "grade": 50};
+  reqGrade("POST", grade, "");
+});
+
+// Edit grade
+document.getElementById("editGrade").addEventListener("click", function() {
+  let name = "Han Solo";
+  let grade = {"grade": 100};
+  reqGrade("PUT", grade, name);
+});
+
+// Delete grade
+document.getElementById("deleteGrade").addEventListener("click", function() {
+  reqGrade("DELETE", null, "Han Solo");
 });
